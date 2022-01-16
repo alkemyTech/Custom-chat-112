14c49f70516e69d756710524f360b4b8b9060e3e
Alejo <75230139+AleYanczuk@users.noreply.github.com>
2022-01-16 15:53:05 -0300

    0T112CHAT-7: Add conversation and messages migration & model files (#4)
    Co-authored-by: AleYanc <aleyanc2000@gmail.com>

--- Gemfile
+++ Gemfile
@@ -8,10 +8,13 @@ ruby '3.0.2'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails', branch: 'main'
 gem 'rails', '~> 6.1.4', '>= 6.1.4.4'
 
+gem 'acts_as_paranoid', '~> 0.7.3' # https://rubygems.org/gems/acts_as_paranoid
 gem 'bcrypt', '~> 3.1', '>= 3.1.16' # https://rubygems.org/gems/bcrypt
 gem 'bootsnap', '>= 1.4.4', require: false # https://rubygems.org/gems/bootsnap
 gem 'dotenv-rails', '~> 2.7', '>= 2.7.6' # https://rubygems.org/gems/dotenv-rails
 gem 'jsonapi-serializer', '~> 2.2' # https://rubygems.org/gems/jsonapi-serializer
+gem 'jwt', '~> 2.3' # https://rubygems.org/gems/jwt
+gem 'pagy', '~> 5.6' # https://rubygems.org/gems/pagy
 gem 'pg', '~> 1.1' # https://rubygems.org/gems/pg
 gem 'puma', '~> 5.0' # https://rubygems.org/gems/puma
 gem 'rest-client', '~> 2.1' # Http client. https://rubygems.org/gems/rest-client
---
+++ app/controllers/api/v1/messages_controller.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class MessagesController < ApplicationController
+      before_action :authorize_request
+      before_action :set_conversation, only: %i[create]
+      before_action :set_message, only: %i[create]
+
+      def create
+        if set_conversation
+          @conversation.users << current_user
+        else
+          current_user.conversations.create!(state: '{"status": "active" }')
+        end
+        @message = @conversation.messages.build(message_params)
+        @message.user = current_user
+        if @message.save
+          render json: MessageSerializer.new(@message), status: :created
+        else
+          render json: @message.errors, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def message_params
+        params.require(:message).permit(:detail, :modified)
+      end
+
+      def set_conversation
+        @conversation = Conversation.find_by(id: params[:conversation_id])
+      end
+
+      def set_message
+        @message = Message.find_by(id: params[:message_id])
+      end
+    end
+  end
+end
