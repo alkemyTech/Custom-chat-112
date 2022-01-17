076e2dd5e8d88614a2b91645b9046962cba3d11c
Victor Jara <Chogo01@hotmail.com>
2022-01-17 19:58:08 -0300

    prueba

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -22,6 +22,37 @@ module Api
       def set_chat
         @chat = Chat.find(params[:chat_id])
       end
+
+      message = "Vete a la mierda"
+
+def word_comparer(string)
+  new_word = []
+  split_word = string.split
+  split_word.each do |word|
+    revised_word = bad_word_detector(word)
+    new_word << revised_word
+    new_word_join = new_word.join(' ')
+  end
+  new_string(new_word)
+end
+
+def new_string(array)
+  revised_string = array.join(' ')
+  puts revised_string
+end
+
+def bad_word_detector(word)
+  bad_words = ["tonto", "cagón", "mierda", "puta"]
+  if bad_words.include?(word)
+    bad_word = bad_words.select { |bad_word| bad_word == word }
+    bad_word_split = bad_word[0].to_s.split(//)
+    censored_word = bad_word_split.map { |x| '*'}
+    censored_word_join = censored_word.join
+  else
+     no_bad_word = word
+  end
+end
+
     end
   end
 end
