63e43fa3bbc844eda824cb94cf6f1854763955ae
Victor Jara <Chogo01@hotmail.com>
2022-01-19 16:06:01 -0300

    OT112-CHAT-14:added bad word detection service

---
+++ app/services/bad_word_detector.rb
@@ -0,0 +1,9 @@
+class BadWordDetector
+  @bad_words = [/tonto/i, /cag[óo]n/i, /mierda/i, /puta/i]
+
+  def detector(string)
+    @bad_words.map do |bad_word|
+      string.gsub!(bad_word, '****')
+    end
+  end
+end
